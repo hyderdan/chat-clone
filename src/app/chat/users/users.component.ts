8919ca834aa993e1f72e43556b2863d7eaeabfc0
Hyder Danish <hyder.danish369@gmail.com>
2024-05-11 18:26:45 +0530

    done

--- src/app/chat/users/users.component.ts
+++ src/app/chat/users/users.component.ts
@@ -1,18 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChatComponent } from '../chat.component';
+import { DataSharingService } from '../../services/data-sharing.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPaperPlane, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [ChatComponent],
+  imports: [ChatComponent, FontAwesomeModule, CommonModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
-  constructor(private UP:ChatComponent ){};
-  getValues = ""
-  getName(){
-    console.log(this.UP.username);
+export class UsersComponent implements OnInit {
+  constructor(private dataSharing: DataSharingService, private router: Router,) { };
+  getValues = "";
+  public Gooback: boolean = true;
+  public faPlane = faPaperPlane;
+  faarrowleft = faArrowLeft;
+
+  ngOnInit(): void {
+    this.getName();
+    this.getShowProfile();
+  }
+  goBAck(params: string) {
+    const back: boolean = false;
+    this.dataSharing.handleUsersm(back);
+    this.router.navigate([params]);
+
+  }
+  getName() {
+    this.dataSharing.currentUsername.subscribe(username => {
+      this.getValues = username;
+    })
+  }
+  getShowProfile() {
+    this.dataSharing.currentShowProfile.subscribe(showProfile => {
+      this.Gooback = showProfile;
+      console.log(showProfile);
+    })
   }
 
 }
