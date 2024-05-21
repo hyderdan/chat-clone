3930be8c3d553383b568c85ab0bbf7d527dcca39
Hyder Danish <hyder.danish369@gmail.com>
2024-05-21 16:38:26 +0530

    done

---
+++ src/app/services/postdatas.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostdatasService {
+
+  constructor(private http: HttpClient) { }
+
+  postDatas(username: string, phoneNo: number, password: string, confirmPass: string) {
+    return this.http.post<any>('http://localhost:3000/users/sigUp', { username, phoneNo, password, confirmPass })
+  }
+
+
+}
+
