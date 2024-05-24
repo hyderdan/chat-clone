493c63cb30dd4bad4b3ca55ca5db1e2c086cb1c3
Hyder Danish <hyder.danish369@gmail.com>
2024-05-24 16:23:17 +0530

    done

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormsModule, } from '@angular/forms';
+import { PostdatasService } from '../services/postdatas.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
+  providers: [PostdatasService],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  toggleloader = true;
+  searachInput: string = ""
+  users: any[] = [];
+    ngOnInit(): void {
+    }
+  constructor(private SearchUser: PostdatasService) { }
+
+  handleSearch() {
+    this.toggleloader = false
+    setTimeout(() => {
+      this.toggleloader = true
+      this.SearchUser.SearchUser(this.searachInput).subscribe(
+        res => {
+          console.log(res.data); 
+          this.users = res.data;
+               }
+      )
+    }, 500);
+     
+    
+  }
 
 }
